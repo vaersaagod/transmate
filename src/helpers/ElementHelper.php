f0ae72a5486918d05ef8135fe211ca0b073dfd6b
Andre Elvan <aelvan@gmail.com>
2025-02-26 14:25:23 +0100

    Fixed an issue related to enabling entries for sections with custom propagation

--- src/helpers/ElementHelper.php
+++ src/helpers/ElementHelper.php
@@ -18,17 +18,12 @@ class ElementHelper
             if ($element instanceof Entry && $element->sectionId !== null && $element->section->propagationMethod === PropagationMethod::Custom) {
                 // The section has custom propagation, ie "Let each entry choose...".
                 // The entry doesn't exist in the target site, so let's create it.
-                $sitesEnabled = $element->getEnabledForSite();
                 $targetSiteId = $site->id;
                 
-                if (!isset($sitesEnabled[$targetSiteId])) {
-                    $sitesEnabled[$targetSiteId] = $element->enabledForSite;
-                } else {
-                    $sitesEnabled = [
-                        $element->site->id => $element->enabledForSite,
-                        $targetSiteId => $element->enabledForSite,
-                    ];
-                }
+                $sitesEnabled = [
+                    $element->site->id => $element->enabledForSite,
+                    $targetSiteId => $element->enabledForSite,
+                ];
 
                 $element->setEnabledForSite($sitesEnabled);
 
@@ -49,7 +44,7 @@ class ElementHelper
                 // Duplicates element to new site. Which is necessary for nested entries to work.
                 // But this probably means that an element can be duplicated and translated for a
                 // section that doesn't propagate. In whichcase there will be duplicates if it's
-                // done more than once. More of a cave-at and/or feature, than a bug? 
+                // done more than once. More of a cave-at and/or feature, than a bug?
                 
                 $targetElement = \Craft::$app->elements->duplicateElement($element, ['siteId' => $site->id]);
             }
