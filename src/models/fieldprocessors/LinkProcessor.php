f136af1cb781263607cf1a7bcc51665d96387637
Andre Elvan <aelvan@gmail.com>
2025-05-13 11:21:15 +0200

    Added support for translating title and aria label for link fields

--- src/models/fieldprocessors/LinkProcessor.php
+++ src/models/fieldprocessors/LinkProcessor.php
@@ -43,6 +43,14 @@ class LinkProcessor extends Model implements ProcessorInterface
             $val['label'] = $translator->translate($val['label']);
         }
         
+        if (!empty($val['ariaLabel'])) {
+            $val['ariaLabel'] = $translator->translate($val['ariaLabel']);
+        }
+        
+        if (!empty($val['title'])) {
+            $val['title'] = $translator->translate($val['title']);
+        }
+        
         // TODO: Should we dive into elements and try to fix links to elements in different sites...? Probably not.
         // Could maybe piggyback on the code for fixing refs (that was added later) 
         
